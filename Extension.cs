27bf09a48d19a9d4b7cf91c6204bff76149872e7
nihatkhidirov <nihatmkh@code.edu.az>
2024-03-12 16:35:00 +0400

    First Commit

---
+++ Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Extension
+{
+    public static class Extension
+    {
+       public static int Faktorial(this int x)
+        {
+            int ia = 1;
+            for (int i = 1; i < x; i++)
+            {
+                ia=ia*i;
+
+            }
+            return ia;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Enum_Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 6;
+            Console.WriteLine(x.Faktorial()); 
+        }
+    }
+}
